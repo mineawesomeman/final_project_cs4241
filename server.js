38e565e1062f9d1931789b4911b1f8910617716a
Addy <boxturtle2003@gmail.com>
2023-10-06 10:41:04 -0400

    Update server.js Oauth
    beginning to integrate git oauth for users

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 import express from 'express';
 import ViteExpress from 'vite-express';
+import passport from 'passport';
+import GitHubStrategy from 'passport-github';
 
 const app = express();
 
 app.use(express.json());
 
-// Sample data (to be replaced with database connection)
+passport.use(new GitHubStrategy({
+  clientID: "ee68f2a2eb45c1602179",
+  clientSecret: "c94bc95b579f85d6fb3836ffd9f675491e4a5519",
+  callbackURL: "http://localhost:3000/auth/github/callback"
+},
+function(accessToken, refreshToken, profile, done) {
+  //TODO Save user profile data into DB
+  return done(null, profile);
+}));
+
+app.get('/auth/github', passport.authenticate('github'));
+
+app.get('/callback', 
+  passport.authenticate('github', { failureRedirect: '/' }),
+  function(req, res) {
+    // Successful authentication
+    res.redirect('/');
+  });
+
+
+
+// replace with db connection
 const data = [];
 
 // Add function
