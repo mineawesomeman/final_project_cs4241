12e5d1754152da847a67102f9761fccccfe314cf
Colinm1215 <colinm1215@gmail.com>
2023-10-08 18:13:49 -0400

    Revert "Merge branch 'chatroom' into dbfunctions"
    This reverts commit 2f8a3e5fb30d441f1d8b8ca3cf997c185e686557, reversing
    changes made to 1930ee230fdb9643ae857bd0cd71f93e6fbf9506.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,20 @@
-import { useState } from 'react';
-import Chat from './Chat';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 import LoginComponent from './LoginComponent';
 
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [selectedRoom, setSelectedRoom] = useState(null);
+  const [count, setCount] = useState(0)
 
 return (
     <>
       <LoginComponent />
-      <div className="card">
-        <p>Select a chatroom:</p>
-        <select value={selectedRoom} onChange={(e) => setSelectedRoom(e.target.value)}>
-          <option value="">-- Select a room --</option>
-          <option value="Room1">Room1</option>
-          <option value="Room2">Room2</option>
-          {/* Add more rooms as needed */}
-        </select>
-        {selectedRoom && <Chat room={selectedRoom} />}
-      </div>
+      {/* ... other components ... */}
     </>
   );
 }
 
-export default App;
+export default App
+
