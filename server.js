6922d72e7f1a1521567dfbcd856a906a59d99ac8
Addy <boxturtle2003@gmail.com>
2023-10-08 12:55:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ import ViteExpress from 'vite-express'
 import axios from 'axios'
 const clientSecret = process.env.GITHUB_CLIENT_SECRET;
 
+const app = express();
 
-const app = express()
+// In-memory storage for user data
+const users = [];
+
 app.get('/auth/github/callback', async (req, res) => {
+  console.log('Received GitHub callback request.');
+
   const code = req.query.code;
 
-  
+  console.log('Exchanging GitHub code for access token...');
+  // Exchange code for access token
   const response = await axios.post('https://github.com/login/oauth/access_token', {
     client_id: 'ee68f2a2eb45c1602179',
     client_secret: clientSecret,
@@ -20,7 +26,9 @@ app.get('/auth/github/callback', async (req, res) => {
   });
 
   const accessToken = response.data.access_token;
+  console.log('Received access token from GitHub.');
 
+  console.log('Fetching user data from GitHub...');
   // Fetch user data from GitHub
   const userResponse = await axios.get('https://api.github.com/user', {
     headers: {
@@ -28,15 +36,27 @@ app.get('/auth/github/callback', async (req, res) => {
     }
   });
 
-  const userData = userResponse.data;
+  const githubUserData = userResponse.data;
+  console.log('Received user data from GitHub:', githubUserData);
 
-  //TODO store the user data in your database and create a session or JWT for the user
+  // Store user data in local storage
+  const user = {
+    user_id: users.length + 1, // Simple incrementing ID for now
+    github_oauth_id: githubUserData.id,
+    username: githubUserData.login,
+    display_name: githubUserData.name || githubUserData.login,
+    joined_date: new Date().toISOString()
+  };
+  users.push(user);
+
+  console.log('Stored user in local storage:', user);
 
   res.redirect('/'); // Redirect user back to app
+  console.log('Redirected user back to app.');
 });
 
+app.use(express.json());
 
-app.use( express.json() )
-
-
-ViteExpress.listen( app, 3000 )
+ViteExpress.listen(app, 3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
