a4b6a008632ddcd47bc657b7711a10ae1fdd9800
Addy <boxturtle2003@gmail.com>
2023-10-08 11:41:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,58 +1,42 @@
-import express from 'express';
-import ViteExpress from 'vite-express';
-import passport from 'passport';
-import GitHubStrategy from 'passport-github';
+import express from  'express'
+import ViteExpress from 'vite-express'
+import axios from 'axios'
+const clientSecret = process.env.GITHUB_CLIENT_SECRET;
 
-const app = express();
 
-app.use(express.json());
+const app = express()
+app.get('/auth/github/callback', async (req, res) => {
+  const code = req.query.code;
 
-passport.use(new GitHubStrategy({
-  clientID: "ee68f2a2eb45c1602179",
-  clientSecret: "c94bc95b579f85d6fb3836ffd9f675491e4a5519",
-  callbackURL: "http://localhost:3000/auth/github/callback"
-},
-function(accessToken, refreshToken, profile, done) {
-  //TODO Save user profile data into DB
-  return done(null, profile);
-}));
-
-app.get('/auth/github', passport.authenticate('github'));
-
-app.get('/callback', 
-  passport.authenticate('github', { failureRedirect: '/' }),
-  function(req, res) {
-    // Successful authentication
-    res.redirect('/');
+  
+  const response = await axios.post('https://github.com/login/oauth/access_token', {
+    client_id: 'ee68f2a2eb45c1602179',
+    client_secret: clientSecret,
+    code: code
+  }, {
+    headers: {
+      'Accept': 'application/json'
+    }
   });
 
+  const accessToken = response.data.access_token;
 
-
-// replace with db connection
-const data = [];
-
-// Add function
-app.post('/add', (req, res) => {
-    const item = req.body;
-
-    // TODO: Add database connection here to save the item
-    data.push(item);
-
-    res.status(200).send({ message: 'Item added successfully!' });
-});
-
-// Delete function
-app.delete('/delete/:id', (req, res) => {
-    const { id } = req.params;
-
-    // TODO: Add database connection here to delete the item by its ID
-    const index = data.findIndex(item => item.id === id);
-    if (index !== -1) {
-        data.splice(index, 1);
-        res.status(200).send({ message: 'Item deleted successfully!' });
-    } else {
-        res.status(404).send({ message: 'Item not found!' });
+  // Fetch user data from GitHub
+  const userResponse = await axios.get('https://api.github.com/user', {
+    headers: {
+      'Authorization': `token ${accessToken}`
     }
+  });
+
+  const userData = userResponse.data;
+
+  //TODO store the user data in your database and create a session or JWT for the user
+
+  res.redirect('/'); // Redirect user back to app
 });
 
-ViteExpress.listen(app, 3000);
+
+app.use( express.json() )
+
+
+ViteExpress.listen( app, 3000 )
