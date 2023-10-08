1930ee230fdb9643ae857bd0cd71f93e6fbf9506
Addy <boxturtle2003@gmail.com>
2023-10-08 14:53:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -39,7 +39,25 @@ app.get('/auth/github/callback', async (req, res) => {
   const githubUserData = userResponse.data;
   console.log('Received user data from GitHub:', githubUserData);
 
-  // Store user data in local storage
+  /*
+Will stay in callback
+const user = {
+    github_oauth_id: githubUserData.id,
+    username: githubUserData.login,
+    display_name: githubUserData.name || githubUserData.login,
+    joined_date: new Date().toISOString()
+  };
+
+  try {
+    const savedUser = await addUserToDatabase(user);
+    console.log('User saved to database:', savedUser);
+  } catch (err) {
+    console.error('Error saving user to database:', err);
+  }
+
+  */
+
+  // Store user data in local storage- will be deleted
   const user = {
     github_oauth_id: githubUserData.id,
     username: githubUserData.login,
@@ -49,6 +67,55 @@ app.get('/auth/github/callback', async (req, res) => {
   users.push(user);
 
   console.log('Stored user in local storage:', user);
+  //end of deleted portion
+
+  /*
+  Digital Ocean replacement-connection template for postgresql
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'your_db_username',
+  host: 'your_db_host',
+  database: 'your_db_name',
+  password: 'your_db_password',
+  port: your_db_port
+});
+
+
+  */
+
+  /*
+  Adding new user data to db after oauth
+async function addUserToDatabase(user) {
+  try {
+    const query = `
+      INSERT INTO users(github_oauth_id, username, display_name, joined_date)
+      VALUES($1, $2, $3, $4)
+      RETURNING *;
+    `;
+
+    const values = [user.github_oauth_id, user.username, user.display_name, user.joined_date];
+
+    const result = await pool.query(query, values);
+    return result.rows[0]; // Returns the inserted user
+  } catch (err) {
+    console.error('Error inserting user into database:', err);
+    throw err;
+  }
+}
+
+  */
+
+  /*
+SQL CODE for creating table:
+CREATE TABLE users (
+  github_oauth_id BIGINT UNIQUE NOT NULL PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  display_name VARCHAR(255),
+  joined_date TIMESTAMP NOT NULL
+);
+
+  */
 
   res.redirect('/'); // Redirect user back to app
   console.log('Redirected user back to app.');
